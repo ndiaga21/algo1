03e28f37718a4423c76394dcda5f41fc7b6b58f4
ndiaga21 <ndiagadiengs1@gmail.com>
2020-01-24 00:12:25 +0100

    tri_bulle

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List list = new ArrayList();
-
+        List<Integer> list = Arrays.asList(1,3,5,4,2,8,9,0,7,6);
+        System.out.println(list);
+        Tri_Bulle tri_bulle = new Tri_Bulle();
+        ArrayList listTrie = new ArrayList<>();
+        listTrie.addAll(list);
+        tri_bulle.compare(listTrie);
+        System.out.println(listTrie);
     }
 }
